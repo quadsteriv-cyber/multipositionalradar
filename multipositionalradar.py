c56ec30ce16445e9d9073e95749f881b16d23ff7
quadsteriv-cyber <quadsteriv@gmail.com>
2025-08-13 16:26:12 -0500

    Update multipositionalradar.py

--- multipositionalradar.py
+++ multipositionalradar.py
@@ -473,7 +473,10 @@ def create_comparison_radar_chart(players_data, radar_config):
     ax.set_ylim(0, 100)
     ax.set_xticks(angles[:-1])
     ax.set_xticklabels(labels, size=9, color='white')
-    ax.set_rgrids([20, 40, 60, 80], color='gray', linestyle='--')
+    ax.set_rgrids([20, 40, 60, 80])
+    for gridline in ax.yaxis.get_gridlines():
+        gridline.set_color('gray')
+        gridline.set_linestyle('--')
     ax.set_title(radar_config['name'], size=16, weight='bold', y=1.12, color='white')
     ax.legend(loc='upper right', bbox_to_anchor=(1.6, 1.15), labelcolor='white', fontsize=10)
 
